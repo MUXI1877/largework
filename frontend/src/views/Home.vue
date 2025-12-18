7ae473d0f1ab3c2d48432ae7e7f6556b5e3ce9e4
李源 <2130185192@qq.com>
2025-12-18 11:56:27 +0800

    进行了权限配置的修改

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <el-card>
+      <h2>欢迎使用权限管理平台</h2>
+      <p>请选择左侧菜单进入相应功能。</p>
+    </el-card>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.home {
+  height: 100%;
+}
+h2 {
+  margin: 0 0 12px 0;
+}
+p {
+  margin: 0;
+  color: #666;
+}
+</style>
+
